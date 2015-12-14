9b77e85e5642e6c3851a9161ce9304ebb1fdb852
adilbaig <adil.baig@aidezigns.com>
2015-12-14 18:59:13 +0530

    Init

---
+++ src/app.d
@@ -0,0 +1,25 @@
+import luad.all;
+
+void main()
+{
+	auto lua = new LuaState;
+	lua.openLibs();
+
+	lua.doString(`print("Calling the lua.doString function")`);
+
+	auto print = lua.get!LuaFunction("print");
+	print("hello, world!");
+	
+	
+	lua[1] = true;
+	lua["test"] = 1.5;
+	lua["globals"] = lua.globals;
+	lua["array"] = [2];
+	lua.doString(`
+	    -- note that _G is a standard loopback reference to the global table
+	    assert(test == 1.5)
+	    assert(globals == _G)
+	    assert(_G[1] == true)
+	    assert(array[1] == 2.5)
+	`);
+}
