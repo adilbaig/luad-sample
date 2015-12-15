7e5b8c5fa50086e64527b85fd0eaa940f4c44e21
adilbaig <adil.baig@aidezigns.com>
2015-12-15 13:59:06 +0530

    More

--- src/app.d
+++ src/app.d
@@ -13,7 +13,15 @@ void main()
 	auto print = lua.get!LuaFunction("print");
 	print("hello, world!");
 	
-	
+	/*
+	 One problem with having a common LuaState is that it needs to be initialized on every request. Otherwise requests from one user 
+	 will override requests from another user.
+	 
+	 Or, preopen a set of LuaStates and reset their globals to zero whenever a new connection is opened. 
+	 Is this more overhead than required?
+	 
+	 Generally, there is a problem of security if LuaState can be written to. This needs to be checked.
+	 */
 	lua[1] = true;
 	lua["test"] = 1.5;
 	lua["globals"] = lua.globals;
@@ -24,5 +32,35 @@ void main()
 	    assert(globals == _G)
 	    assert(_G[1] == true)
 	    assert(array[1] == 2)
+	    
+	    x = 10
+	    local i = 1        -- local to the chunk
+	    
+	    while i<=x do
+	      local x = i*2    -- local to the while body
+	      print(x)         --> 2, 4, 6, 8, ...
+	      i = i + 1
+	    end
+	    
+	    if i > 20 then
+	      local x          -- local to the "then" body
+	      x = 20
+	      print(x + 2)
+	    else
+	      print(x)         --> 10  (the global one)
+	    end
+	    
+	    print(x)
+	    
+	    local a = 1
+		local b = 2
+		local c = 3
+	    do
+	      local a2 = 2*a
+	      local d = sqrt(b^2 - 4*a*c)
+	      x1 = (-b + d)/a2
+	      x2 = (-b - d)/a2
+	    end          -- scope of a2 and d ends here
+	    print(x1, x2)
 	`);
 }
